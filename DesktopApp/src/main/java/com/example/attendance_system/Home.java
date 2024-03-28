875d6f06aaea98573d230397127eac355a5e3298
zm127 <68487053+zm127@users.noreply.github.com>
2024-03-28 13:33:08 -0500

    Add files via upload

---
+++ DesktopApp/src/main/java/com/example/attendance_system/Home.java
@@ -0,0 +1,46 @@
+package com.example.attendance_system;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Home extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("home-view.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/attendanceDB", "root", "Icecream123");
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from student");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("UTD_ID"));
+            }
+
+            launch();
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
